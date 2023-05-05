0447e0b3827834c543e98b79bcf79177341c8d72
ParbhatKataria1 <varshagagangandhi@gmail.com>
2023-05-06 02:47:03 +0530

    done

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const { user } = require('./route/user.route');
+const mongoose = require("mongoose");
+const { user } = require("./route/user.route");
+const cors = require("cors");
+app.use(cors());
+require("dotenv").config();
 app.use(express.json());
 
-app.use('/user', user)
+app.use("/user", user);
 
-app.listen(4500, async(req, res)=>{
-    try {
-        await mongoose.connect('mongodb+srv://parbhat:parbhat@cluster0.ejzfwgs.mongodb.net/user?retryWrites=true&w=majority')
-        console.log('database is running')
-    } catch (error) {
-        console.log('database is not running')
-    }
-})
+app.listen(process.env.port, async (req, res) => {
+  try {
+    await mongoose.connect(process.env.mongoURL);
+    console.log("database is running");
+  } catch (error) {
+    console.log("database is not running");
+  }
+});
