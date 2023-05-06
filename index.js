57112af8c925ec87d6994e516b9eaf97cae7a7bd
ParbhatKataria1 <varshagagangandhi@gmail.com>
2023-05-06 10:20:31 +0530

    done

--- index.js
+++ index.js
@@ -3,11 +3,36 @@ const app = express();
 const mongoose = require("mongoose");
 const { user } = require("./route/user.route");
 const cors = require("cors");
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 app.use(cors());
 require("dotenv").config();
 app.use(express.json());
 
 app.use("/user", user);
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Hello World",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        url: "http://localhost:4500", // change
+      },
+    ],
+  },
+  apis: ["./route/*.js"], // change files containing annotations as above
+};
+
+const openapiSpecification = swaggerJSDoc(options);
+app.use(
+  "/documentations",
+  swaggerUi.serve,
+  swaggerUi.setup(openapiSpecification)
+);
 
 app.listen(process.env.port, async (req, res) => {
   try {
--- route/user.route.js
+++ route/user.route.js
@@ -2,6 +2,41 @@ const express = require("express");
 const { Find } = require("../model/user.model");
 const user = express.Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: this is auto generated
+ *         name:
+ *           type: string
+ *           description: this is the name of the user
+ *         age:
+ *           type: integer
+ *           description: this is the age of the user
+ *
+ */
+
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: It is used to get the details of the users.
+ *     responses:
+ *       200:
+ *         description: The request was being made is successfull
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               item:
+ *                 $ref: "#/components/schemas/User"
+ */
+
 user.get("/", async (req, res) => {
   try {
     let data = await Find.find();
